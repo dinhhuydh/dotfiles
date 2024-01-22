4c97b9d3f218efdb9efb0e0a2193e8959b96096e
dinhhuydh <dinhhuydh@gmail.com>
2024-01-22 16:50:09 +0700

    Ctrl+j/k on telescope

--- init.lua
+++ init.lua
@@ -420,12 +420,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
+local actions = require "telescope.actions"
 require('telescope').setup {
   defaults = {
     mappings = {
       i = {
         ['<C-u>'] = false,
         ['<C-d>'] = false,
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous
       },
     },
   },
